2aeafeaa6dae852d3c445e465506c99717d54f23
wc543 <wc543@drexel.edu>
2025-02-11 19:30:57 -0500

    updated register component

---
+++ starter/front/src/Components/Register.tsx
@@ -0,0 +1,13 @@
+import "./Register.css";
+import { useState } from "react";
+import { getAxiosErrorMessages } from "./utils";
+
+function Register() {
+    return (
+        <>
+        <h2>Register</h2>
+        </>
+    )
+}
+
+export default Register;
